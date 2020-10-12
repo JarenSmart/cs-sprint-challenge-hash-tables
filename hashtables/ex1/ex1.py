6bba88737c8b97ec46bca0047e36b1842b5ff2d9
jaren <jarensmart@yahoo.com>
2020-10-11 21:33:56 -0400

    Finished ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,14 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    cache = {}  # create table
+    # iterate with enumerate
+    for index, v in enumerate(weights):
+        if limit - v in cache:  # check in cache/hashtable for weight limit/value
+            # get index of second value in weighed pair
+            weighed_pair = cache.get(limit-v), index
+            # returns weights in proper order
+            return tuple(sorted(weighed_pair, reverse=True))
+        else:
+            # create entry if one is not in cache/hashtable
+            cache[v] = index
 
-    return None
+    return None  # return None if weighed pair doesn't exist
